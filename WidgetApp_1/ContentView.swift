507bf2820ba98224fced2b307c922363f192ef94
Emin <38878208+EMINBARI@users.noreply.github.com>
2020-08-05 10:16:59 +0300

    Initial Commit

---
+++ WidgetApp_1/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WidgetApp_1
+//
+//  Created by Emin Bari on 05.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
